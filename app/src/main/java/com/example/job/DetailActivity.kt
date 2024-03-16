dfc52d21d442ec52ef2ebd84240792d008bc24eb
Danny syed <Dannysyed20@gmail.com>
2024-03-16 10:45:46 -0400

    Adding connection button in DetailActivity

--- app/src/main/java/com/example/job/DetailActivity.kt
+++ app/src/main/java/com/example/job/DetailActivity.kt
@@ -2,6 +2,7 @@ package com.example.job
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.firebase.database.FirebaseDatabase
@@ -9,6 +10,8 @@ import android.widget.TextView
 import android.widget.ImageView
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var buttonConnect: Button
+    private var isConnected = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
@@ -16,6 +19,7 @@ class DetailActivity : AppCompatActivity() {
         val textViewDetailName: TextView = findViewById(R.id.textViewDetailName)
         val textViewDetailTitle: TextView = findViewById(R.id.textViewDetailTitle)
         val imageViewDetailPhoto: ImageView = findViewById(R.id.imageViewDetailPhoto)
+        buttonConnect = findViewById(R.id.buttonConnect)
 
         val candidateId = intent.getStringExtra("candidateId")
         Log.d("DetailActivity", "Candidate ID: $candidateId")
@@ -32,8 +36,20 @@ class DetailActivity : AppCompatActivity() {
                     Glide.with(this)
                         .load(candidate.photoUrl)
                         .into(imageViewDetailPhoto)
+                    isConnected = false
+                    updateConnectButton()
                 }
             }
         }
+        buttonConnect.setOnClickListener {
+            isConnected = !isConnected
+            updateConnectButton()
+
+        }
+
+    }
+    private fun updateConnectButton() {
+        val buttonText = if (isConnected) "Disconnect" else "Connect"
+        buttonConnect.text = buttonText
     }
 }
